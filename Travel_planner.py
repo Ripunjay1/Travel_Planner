a945bf54a5e6d78d9e38d89ed70ad5d90dc3c99c
Ripunjay1 <144810605+Ripunjay1@users.noreply.github.com>
2025-02-27 01:05:47 +0530

    Create Travel_planner.py

---
+++ Travel_planner.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.set_page_config(page_title= "Travel Planner")
+st.title("I am your Travel Planner")
+
+genai.configure(api_key = "AIzaSyBUWdkr6uoz9QnoOrDedsK4N0s4aLHGD-c")
+
+sys_prompt=("""You are Travel planner who will ask for three inputs which is Location , Days for how many 
+traveller wants to travel and date. Location and days would the mandotary inputs. After getting response you 
+will look out the information you have and give 3 different responses which would be travel plans. If anyone
+asks a query which is not related to travel planning ask them gentely to ask related question""")
+
+model = genai.GenerativeModel('models/gemini-2.0-pro-exp')
+
+user_prompt = st.text_input("Enter your Details")
+btn_click = st.button("Generate response")
+
+if btn_click ==True:
+    response = model.generate_content(user_prompt)
+    st.write(response.text)
